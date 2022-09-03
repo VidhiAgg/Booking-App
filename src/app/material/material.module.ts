b4058021dd9539a2c4cba20d8408fbd06b1faf03
Vidhi Aggarwal <vidhiaggarwal397@gmail.com>
2022-09-03 15:46:52 +0530

    Added style

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import {MatButtonModule} from "@angular/material/button"
+import {MatIconModule} from "@angular/material/icon"
 
 
 const MateraialComponents=[
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule
 ]
 
 @NgModule({
